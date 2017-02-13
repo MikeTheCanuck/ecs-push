df949392a53fcc09764702e57e64fef3130252ee
Mike Lonergan <mikethecanuck@gmail.com>
2017-02-12 22:42:07 -0800

    fixed a number of pylint warnings

--- user/tests.py
+++ user/tests.py
@@ -14,13 +14,13 @@ class HealthTestCase(TestCase):
         url = reverse('health-list')
         response = self.c.get(url)
         assert response.status_code == 200, \
-            "Expect 200 OK. got: {}" . format (response.status_code)
+            "Expect 200 OK. got: {}" . format(response.status_code)
 
         expected_fields = ["db", "status"]
 
         for field in self.status_fields:
             assert response.json().get("status", {}).get(field, None) == "up", \
-                "Expected field {} to exist" . format (field)
+                "Expected field {} to exist" . format(field)
 
     @patch.object(User.objects, 'first')
     def test_determine_db_status(self, mock_query):
@@ -33,11 +33,11 @@ class HealthTestCase(TestCase):
         status = response.json().get("status", {})
         db_status = status.get('db')
         assert db_status == 'down', \
-            'Expect DB to be down. Got: {}' . format (db_status)
+            'Expect DB to be down. Got: {}' . format(db_status)
 
         status = status.get('status')
         assert status == 'down', \
-            'Expect status to be down. Got: {}' . format (status)
+            'Expect status to be down. Got: {}' . format(status)
 
 class UserAPITestCase(TestCase):
 
@@ -66,22 +66,22 @@ class UserAPITestCase(TestCase):
             "Expect it to return exactly 2 users. Got: {}" . format(num_users)
 
     def test_get_user_returns_correct_fields(self):
-            """GET /user/{pk} returns a user"""
+        """GET /user/{pk} returns a user"""
 
-            expected_fields = ['url', 'username', 'email', 'is_staff']
-            url = reverse("user-detail", args=[self.normal_user.pk])
-            response = self.c.get(url)
+        expected_fields = ['url', 'username', 'email', 'is_staff']
+        url = reverse("user-detail", args=[self.normal_user.pk])
+        response = self.c.get(url)
 
-            assert response.status_code == 200, \
-                "Expect 200 OK. got: {}" . format(response.status_code)
+        assert response.status_code == 200, \
+            "Expect 200 OK. got: {}" . format(response.status_code)
 
-            assert response.json()["is_staff"] == False
-            assert response.json()["username"] == "joe"
-            assert response.json()["email"] == "joe@soap.com"
-            expected_url = "http://testserver/users/{}/" . format(
-                self.normal_user.pk)
-            assert response.json()["url"] == expected_url, \
-                'Expect url to be set. Got: {}' . format(response.json()["url"])
+        assert response.json()["is_staff"] == False
+        assert response.json()["username"] == "joe"
+        assert response.json()["email"] == "joe@soap.com"
+        expected_url = "http://testserver/users/{}/" . format(
+            self.normal_user.pk)
+        assert response.json()["url"] == expected_url, \
+            'Expect url to be set. Got: {}' . format(response.json()["url"])
 
     def tearDown(self):
         for user in User.objects.all():
